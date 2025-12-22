6a4c8843c749a1953e3676a16c91a35dffed46a4
Bri Ward <me@briward.com>
2025-12-22 17:15:38 +0000

    Remove colours from the README badges.

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,4 +1,8 @@
-import { type Context, type Error, ServerError } from "jsr:@raptor/framework@0.9.0";
+import {
+  type Context,
+  type Error,
+  ServerError,
+} from "jsr:@raptor/framework@0.9.0";
 
 import CodeExtractor from "./code-extractor.ts";
 import CodeHighlighter from "./code-highlighter.ts";
